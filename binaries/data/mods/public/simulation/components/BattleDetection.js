5c269ca4b122aab68445c801ccce719e8895e4e6
zoot <zootzootzootzoot@gmail.com>
2012-12-03 14:34:11 +0100

    Basic damage rate tracking.

--- binaries/data/mods/public/simulation/components/BattleDetection.js
+++ binaries/data/mods/public/simulation/components/BattleDetection.js
@@ -8,6 +8,46 @@ BattleDetection.prototype.Schema =
 
 BattleDetection.prototype.Init = function()
 {
+	this.pastAttacks = [];
+	this.interval = 10 * 1000; // Interval over which damage rate should calculated in milliseconds.
+	this.damageRateThreshold = 0.04; // Damage rate at which the player is considered in battle.
+};
+
+BattleDetection.prototype.OnGlobalAttacked = function(msg)
+{
+	var cmpPlayer = Engine.QueryInterface(this.entity, IID_Player);
+	// Only register attacks dealt by myself.
+	var cmpAttackerOwnership = Engine.QueryInterface(msg.attacker, IID_Ownership);
+	if (!cmpAttackerOwnership || cmpAttackerOwnership.GetOwner() != cmpPlayer.GetPlayerID())
+		return;
+	// Don't register attacks dealt against Gaia or invalid player.	
+	var cmpTargetOwnership = Engine.QueryInterface(msg.target, IID_Ownership);
+        if (!cmpTargetOwnership || cmpTargetOwnership.GetOwner() <= 0)
+		return;
+
+	var cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+	var currentTime = cmpTimer.GetTime();
+
+	if (msg.damage)
+		this.pastAttacks.push({time: currentTime, damage: msg.damage});
+
+	var totalDamage = 0;
+	var deltaTime = 0;
+	for (var i = this.pastAttacks.length-1; i >= 0; i--)
+	{
+		deltaTime = currentTime - this.pastAttacks[i].time;
+		if (deltaTime < this.interval)
+			totalDamage += this.pastAttacks[i].damage;
+		else
+			this.pastAttacks.splice(i, 1);
+	}
+
+	if (deltaTime)
+		var damageRate = totalDamage / deltaTime;
+	if (damageRate && damageRate > this.damageRateThreshold)
+		warn("Player "+String(cmpPlayer.GetPlayerID())+" is in battle! (rate:"+String(damageRate)+")");
+	else
+		warn("Player "+String(cmpPlayer.GetPlayerID())+" is at peace. (rate:"+String(damageRate)+")");
 };
 
 Engine.RegisterComponentType(IID_BattleDetection, "BattleDetection", BattleDetection);
