b1e5e5b2cee620bbdce74afc06b4ab04ee3a3479
laxmax <abhijit.nandy@gmail.com>
2013-04-06 02:41:14 +0530

    Added message handling for MT_EntityAttacked in the ICmpMinimap interface

--- binaries/data/mods/public/simulation/components/interfaces/AttackDetection.js
+++ binaries/data/mods/public/simulation/components/interfaces/AttackDetection.js
@@ -1,9 +1,9 @@
 Engine.RegisterInterface("AttackDetection");
 
-// Message of the form { "event": { "position": { "x": 123, "z": 456 }, "time": 1, "target": 123 }.
+// Message of the form { "event": { "target": 123, "position": { "x": 123, "z": 456 }, "time": 1 }.
 // sent when an entity is attacked.
-Engine.RegisterMessageType("EntityAttacked");
+//Engine.RegisterMessageType("EntityAttacked");
 
-// Message of the form { "player": 1, "event": { "position": { "x": 123, "z": 456 }, "time": 1, "target": 123 }.
+// Message of the form { "player": 1, "event": { "target": 123 , "position": { "x": 123, "z": 456 }, "time": 1, }.
 // sent when a new attack is detected.
 Engine.RegisterMessageType("AttackDetected");
