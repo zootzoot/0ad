a4a9d58ea73779f476f46ead5e5001439479072f
zoot <zootzootzootzoot@gmail.com>
2013-05-31 00:54:52 +0200

    Clean up unused EntityAttacked message type.

--- binaries/data/mods/public/simulation/components/interfaces/AttackDetection.js
+++ binaries/data/mods/public/simulation/components/interfaces/AttackDetection.js
@@ -1,9 +1,5 @@
 Engine.RegisterInterface("AttackDetection");
 
-// Message of the form { "event": { "target": 123, "position": { "x": 123, "z": 456 }, "time": 1 }.
-// sent when an entity is attacked.
-//Engine.RegisterMessageType("EntityAttacked");
-
 // Message of the form { "player": 1, "event": { "target": 123 , "position": { "x": 123, "z": 456 }, "time": 1, }.
 // sent when a new attack is detected.
 Engine.RegisterMessageType("AttackDetected");
